2afb25cb77b532282cc6fc043b431f9eb379b033
SirPotatoIV <otoolej33@gmail.com>
2020-03-23 15:47:14 -0500

    Added labels, inputs, and a button to the CreatePost component. Started writing a function for handling the creation of a post and onChange events to the input elements to store the users title and content.

--- src/components/PostCreation/PostCreation.js
+++ src/components/PostCreation/PostCreation.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function PostCreation() {
-  return <div>Post Creation Test</div>;
+export default function PostCreation(props) {
+  const { firestore } = props;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handlePostCreation = async post => {
+    const docRef = await firestore.collection("posts").add(post);
+    console.log(docRef);
+  };
+
+  // Reference to why I used htmlFor instead of for https://stackoverflow.com/questions/22752116/react-ignores-for-attribute-of-the-label-element
+  return (
+    <div>
+      <label htmlFor="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        onChange={({ target }) => {
+          setTitle(target.value);
+        }}
+      ></input>
+      <br></br>
+      <label htmlFor="content">Content:</label>
+      <input type="text" id="content"></input>
+      <button type="button">Create Post</button>
+    </div>
+  );
 }
